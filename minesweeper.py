6bf3b4de2af7222509263eadede12db38d20bd90
Manuel <manuelheller@outlook.com>
2020-07-20 20:41:37 +0200

    progress with add_knowledge function

--- minesweeper.py
+++ minesweeper.py
@@ -229,6 +229,14 @@ class MinesweeperAI():
                 sentence_cells.remove(cell)
         ai.knowledge.append(Sentence(sentence_cells, sentence_count))
         # 4) mark any additional cells as safe or as mines
+        for sentence in self.knowledge:
+            sentence_copy = copy.deepcopy(sentence)
+            if len(sentence_copy.cells) == sentence_copy.count:
+                for cell in sentence_copy.cells:
+                    self.mark_mine(cell)
+            if sentence_copy.count == 0:
+                for cell in sentence_copy.cells:
+                    self.mark_safe(cell)                
 
     def make_safe_move(self):
         """
